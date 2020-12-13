0ed952ca222c82200cecba97ef216675fb9ec69d
saiprakash774 <69646766+saiprakash774@users.noreply.github.com>
2020-12-13 12:05:30 +0530

    Added: addNewContact method to add contacts

--- src/addressbooks/AddressBook.java
+++ src/addressbooks/AddressBook.java
@@ -1,7 +1,13 @@
 package addressbooks;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
 public class AddressBook {
-	public static void main(String args[]) {
+	List<Contacts> ContactsList = new ArrayList<>();
+	
+	public void addNewContact() {
 		Scanner s1 = new Scanner(System.in);
 		System.out.println("Enter firstname of the person: ");
 		String firstname = s1.nextLine();
@@ -16,7 +22,25 @@ public class AddressBook {
 		System.out.println("Ente the zip code of the person");
 		int zip = s1.nextInt();
 		Contacts contacts = new Contacts(firstname, lastname, city, state, phone, zip);
-		System.out.println(contacts.toString());
+		ContactsList.add(contacts);
 	}
 
+	public static void main(String args[]) {
+		Scanner s2 = new Scanner(System.in);
+		AddressBook addressbook = new AddressBook();
+		int value=1;
+		while(value==1) {
+		System.out.println("Enter 1 to add contacts and 2 to Quit");
+		int choice = s2.nextInt();
+		switch (choice) {
+		case 1:
+				addressbook.addNewContact();
+				break;
+		case 2:	value=0;
+				break;
+		default:
+				System.out.println("You Entered wrong Option");
+		}
+	}
+}
 }
